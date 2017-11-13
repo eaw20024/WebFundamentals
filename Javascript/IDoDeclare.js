2c80ab56927a8cef666d8ed680566ddc6bc11087
Eric Washington <eric.washington.me@gmail.com>
2017-11-13 11:12:33 -0500

    Javascript Fundamentals

---
+++ Javascript/IDoDeclare.js
@@ -0,0 +1,18 @@
+let number1 = 12323;
+let number2 = 234241;
+let number3 = 398129283;
+let number4 = -4839381033;
+let string1 = "Hello";
+let string2 = "Good Bye";
+let string3 = "Yea! Yea!";
+let bool1 = true;
+let bool2 = false;
+let undef = undefined;
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(number1 + number2 * number3 - number4);
+console.log(bool1)
+console.log(bool2)
+console.log(undef);
