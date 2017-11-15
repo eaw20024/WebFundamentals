56860f5e95e52efef2cbc95602f623f691d6af5a
Eric Washington <eric.washington.me@gmail.com>
2017-11-15 12:30:43 -0500

    WhileYouWait.js

---
+++ Javascript/WhileYouWait.js
@@ -0,0 +1,18 @@
+var daysUntilBDay = 60;
+
+for(var i = daysUntilBDay; i >= 1; i--){
+
+   if(daysUntilBDay <= 60 && daysUntilBDay >= 30){
+    console.log("Their are " + i + " days until my birthday. Such a long time.....");
+   } else if(daysUntilBDay <= 29 && daysUntilBDay >= 15){ 
+    console.log("Their are " + i + " days until my birthday. SMDH! well its getting closer..");
+   } else if(daysUntilBDay >= 1 || daysUntilBDay <= 14){ 
+    console.log("Their are " + i + " days until my birthday. OMG!! Sooo Close!!!");
+   } 
+}
+
+if (daysUntilBDay) {
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*"); 
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪"); 
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*"); 
+}
