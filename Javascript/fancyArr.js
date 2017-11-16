c0896e108ef66fe1638bccae9a0fe202e21acc64
Eric Washington <eric.washington.me@gmail.com>
2017-11-16 15:30:49 -0500

    fancyArr.js

---
+++ Javascript/fancyArr.js
@@ -0,0 +1,9 @@
+var arr = [ "James", "Jill", "Jane", "Jack" ];
+
+for(var i = 0; i < arr.length; i++){
+  var str = "";
+  var count = 0;
+  var count = count + i;
+  var str = count + " -> " + arr[i];
+  console.log(str);
+}
