87f643146f6893ec1451309503d86b888430ae96
Eric Washington <eric.washington.me@gmail.com>
2017-11-28 11:08:42 -0500

    Image Swap - JQuery

---
+++ Javascript/image_swap_jquery/javascript/jquery.js
@@ -0,0 +1,12 @@
+    var sourceSwap = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
+
+    $(function() {
+        $('img[data-alt-src]').each(function() { 
+            new Image().src = $(this).data('alt-src'); 
+        }).click(sourceSwap, sourceSwap); 
+    });
