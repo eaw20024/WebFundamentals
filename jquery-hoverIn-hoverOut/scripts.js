664e69d73d3e2267efd044e71a72d31392d6157c
Eric Washington <eric.washington.me@gmail.com>
2019-04-25 17:21:10 -0400

    JQuery HoverIn/HoverOut

---
+++ jquery-hoverIn-hoverOut/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('img').hover(function () {
+    var temp = $(this).attr('src')
+    $(this).attr('src', $(this).attr('data-alt-src'))
+    $(this).attr('data-alt-src', temp)
+  })
+})
